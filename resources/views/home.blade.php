5ca246c9e80f4cf837ecdf92dcd3ad2db2ac0b33
Curso Git <marcooperdomo@gmail.com>
2019-07-27 16:06:47 -0500

    Listado de productos con paginación

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Panel Administrativo</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,7 +14,31 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <p>
+                        {{ $products->total() }} registros |
+                        página {{ $products->currentPage() }}
+                        de {{ $products->lastPage() }}
+                    </p>
+
+                    <table class="table table-over table-striped">
+                        <thead>
+                            <tr>
+                                <th width="20px">ID</th>
+                                <th>Nombre del producto</th>
+                                <th>&nbsp;</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($products as $product)
+                                <tr>
+                                    <td width="20px">{{ $product->id }}</td>
+                                    <td>{{ $product->name }}</td>
+                                    <td width="20px"></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {!! $products->render() !!}
                 </div>
             </div>
         </div>
